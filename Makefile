e43b774227fe8946f3250421169673a693c55abc
Shubham Tomar <tomarshubham@gmail.com>
2021-02-21 19:39:46 +0530

    Code reorganisation and corresponding Makefile changes

--- Makefile
+++ Makefile
@@ -1,39 +1,32 @@
-# include makefile.conf
-# NAME=blink
-# STARTUP_DEFS=-D__STARTUP_CLEAR_BSS -D__START=main
-
-# LDSCRIPTS=-L. -L$(BASE)/ldscripts -T .compilation_scripts/linker.ld
-# LFLAGS=$(USE_NANO) $(USE_NOHOST) $(LDSCRIPTS) $(GC) $(MAP)
-
-# $(NAME)-$(CORE).axf: src/$(NAME).c $(STARTUP)
-# 	$(CC) $^ $(CFLAGS) $(LFLAGS) -o $@
-
-# clean: 
-# 	rm -f $(NAME)*.axf *.map *.o
-
 CC=arm-none-eabi-gcc
 CPU=cortex-m4
-CFLAGS=-c -mcpu=$(CPU) -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -ffunction-sections -fdata-sections -Os
 OUT=output
+INCLUDE=inc
+OBJECT_FILES=objs
+LIBS=libs
+CFLAGS=-c -mcpu=$(CPU) -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -ffunction-sections -fdata-sections -Os -I$(INCLUDE)
 LINKER=arm-none-eabi-ld
-LINKER_SCRIPT=.compilation_scripts/linker.ld
+LINKER_SCRIPT=.compilation_scripts/TM4C123GH6PM.ld
 LFLAGS=--gc-section -T $(LINKER_SCRIPT)
-
 ICDI=/dev/ttyACM0
 #ICDI=0E22F2D3
-
 all: $(OUT)/blink.bin
-
 flash: $(OUT)/blink.bin
 	lm4flash -S $(ICDI) $^
 
-main.o: src/blink.c
+$(OBJECT_FILES)/main.o: src/blink.c
+	@mkdir -p $(@D)
 	$(CC) $(CFLAGS) $^ -o $@
 
-startup.o: src/startup.c
+$(OBJECT_FILES)/startup.o: $(LIBS)/startup.c
+	@mkdir -p $(@D)
 	$(CC) $(CFLAGS) $^ -o $@
 
-$(OUT)/blink.elf: main.o startup.o
+$(OBJECT_FILES)/gpio.o: $(LIBS)/gpio.c
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) $^ -o $@	
+
+$(OUT)/blink.elf: $(OBJECT_FILES)/main.o $(OBJECT_FILES)/startup.o $(OBJECT_FILES)/gpio.o
 	@mkdir -p $(@D)
 	$(LINKER) $(LFLAGS) $^ -o $@
 	rm $^
@@ -43,3 +36,4 @@ $(OUT)/blink.bin: $(OUT)/blink.elf
 
 clean:
 	rm -rf $(OUT)
+	rm -rf $(OBJECT_FILES)
--- src/blink.c
+++ src/blink.c
@@ -1,37 +1,9 @@
+#include <gpio.h>
 
-#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
-#define GPIO_PORTF_DATA_BITS_R  ((volatile unsigned long *)0x40025000)
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_IS_R         (*((volatile unsigned long *)0x40025404))
-#define GPIO_PORTF_IBE_R        (*((volatile unsigned long *)0x40025408))
-#define GPIO_PORTF_IEV_R        (*((volatile unsigned long *)0x4002540C))
-#define GPIO_PORTF_IM_R         (*((volatile unsigned long *)0x40025410))
-#define GPIO_PORTF_RIS_R        (*((volatile unsigned long *)0x40025414))
-#define GPIO_PORTF_MIS_R        (*((volatile unsigned long *)0x40025418))
-#define GPIO_PORTF_ICR_R        (*((volatile unsigned long *)0x4002541C))
-#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
-#define GPIO_PORTF_DR2R_R       (*((volatile unsigned long *)0x40025500))
-#define GPIO_PORTF_DR4R_R       (*((volatile unsigned long *)0x40025504))
-#define GPIO_PORTF_DR8R_R       (*((volatile unsigned long *)0x40025508))
-#define GPIO_PORTF_ODR_R        (*((volatile unsigned long *)0x4002550C))
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_PDR_R        (*((volatile unsigned long *)0x40025514))
-#define GPIO_PORTF_SLR_R        (*((volatile unsigned long *)0x40025518))
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
-#define GPIO_PORTF_ADCCTL_R     (*((volatile unsigned long *)0x40025530))
-#define GPIO_PORTF_DMACTL_R     (*((volatile unsigned long *)0x40025534))
-
-#define SWITCH_TWO (*((volatile unsigned long *)0x40025004))
-#define RED_LED    (*((volatile unsigned long *)0x40025008))
-#define BLUE_LED   (*((volatile unsigned long *)0x40025010))
-#define GREEN_LED  (*((volatile unsigned long *)0x40025020))
-#define SWITCH_ONE (*((volatile unsigned long *)0x40025040))
-
+#define RED 0x02
+#define BLUE 0x04
+#define GREEN 0x08
+#define LED_OFF 0x00
 
 void wait(unsigned long value)
 {
@@ -44,38 +16,22 @@ void wait(unsigned long value)
 }
 
 
-
-
-void PortF_Init(void){ 
-  volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= 0x00000020;     // 1) activate clock for Port F
-  delay = SYSCTL_RCGC2_R;           // allow time for clock to start
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
-  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
-  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog on PF
-  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) PCTL GPIO on PF4-0
-  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 in, PF3-1 out
-  GPIO_PORTF_AFSEL_R = 0x00;        // 6) disable alt funct on PF7-0
-  GPIO_PORTF_PUR_R = 0x11;          // enable pull-up on PF0 and PF4
-  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital I/O on PF4-0
-}
-
 int main(void)
 {	
     PortF_Init();
     while (1)
     {
-		GPIO_PORTF_DATA_R = 0x02;
+		GPIO_PORTF_DATA_R = RED;
 		wait(500000);
-		GPIO_PORTF_DATA_R = 0x00;
+		GPIO_PORTF_DATA_R = LED_OFF;
 		wait(200000);
-		GPIO_PORTF_DATA_R = 0x04;
+		GPIO_PORTF_DATA_R = GREEN;
 		wait(500000);
-		GPIO_PORTF_DATA_R = 0x00;
+		GPIO_PORTF_DATA_R = LED_OFF;
 		wait(200000);
-		GPIO_PORTF_DATA_R = 0x08;
+		GPIO_PORTF_DATA_R = BLUE;
 		wait(500000);
-		GPIO_PORTF_DATA_R = 0x00;
+		GPIO_PORTF_DATA_R = LED_OFF;
 		wait(200000);
     }
 }
